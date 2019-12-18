8ba2aa7c6ceb4e2ef5cf8cbfba44b3ad745ed9c7
ArisGlobal <56895512+ArisGloba1@users.noreply.github.com>
2019-12-18 12:05:35 -0500

    Update create_some_files.sh

--- create_some_files.sh
+++ create_some_files.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-mkdir some-files
-echo "file1" > some-files/file1
-echo "file2" > some-files/file2
-echo "file3" > some-files/file3
-echo "file4" > some-files/file4
+mkdir some-files01
+echo "file1" > some-files01/file1
+echo "file2" > some-files01/file2
+echo "file3" > some-files01/file3
+echo "file4" > some-files01/file4
 
 ls some-files/*
+cp -i some-files/* /home/ubuntu/concourse/ci-pipe
