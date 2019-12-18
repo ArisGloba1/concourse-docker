07be6429101a84324f70aaa398fd6bc7b90a258f
ArisGlobal <56895512+ArisGloba1@users.noreply.github.com>
2019-12-18 13:47:43 -0500

    update the list file

--- create_some_files.sh
+++ create_some_files.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-mkdir some-files
-echo "file1" > some-files/file1
-echo "file2" > some-files/file2
-echo "file3" > some-files/file3
-echo "file4" > some-files/file4
+mkdir AG-Dir
+echo "ag-file1" > AG-Dir/ag-file1
+echo "ag-file2" > AG-Dir/ag-file2
+echo "ag-file3" > AG-Dir/ag-file3
+echo "ag-file4" > AG-Dir/ag-file4
 
-ls some-files/*
+ls AG-Dir/*
 hostname -i
+Cat /etc/hosts
 pwd
